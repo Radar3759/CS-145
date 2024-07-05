c3dac4b063b1f242eb4d7217673e9f414f59bc72
Radar3759 <chainmailgirl@gmail.com>
2024-07-04 17:26:24 -0700

    -add lab 2

---
+++ Lab_2/HeartRates/src/HeartRateCalc.java
@@ -0,0 +1,60 @@
+/* 
+Student: Clarissa Lacefield
+Class: CS145
+Lab: Lab 2 Heart Rate Calculator
+Extra credit: made the current year auto populate
+*/
+
+// allows for input
+import java.util.Scanner;
+
+public class HeartRateCalc {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // allows for input
+
+        //Chap 7.2 white book
+        //create a HeartRates object and assign it to myHeartRates
+        HeartRates myHeartRates = new HeartRates();
+            //introduce HeartRate
+            System.out.printf("%n~ Welcome to Target Heart Rate Calculator ~%n");
+            //ask the user to input their first name
+            System.out.printf("Enter your first name: ");
+            //assigns the input to var firstName
+            String firstName = input.nextLine();
+            //assigns firstName to fName
+            myHeartRates.setFName(firstName);
+            //ask the user to input their last name
+            System.out.printf("Enter your last name: ");
+            //assigns the input to var lastName
+            String lastName = input.nextLine();
+            //assigns lastName to lName
+            myHeartRates.setLName(lastName);
+            //ask the user to input their year of birth
+            System.out.printf("Enter the year you were born YYYY: ");
+            //assigns the input to var birthYear
+            int birthYear = input.nextInt();
+            myHeartRates.setBirthYear(birthYear);
+
+            //assigns the current year manually to a var
+            int currentYear = 2024;
+            // this seems redundant, but it's working
+            myHeartRates.setCurrentYear(currentYear);
+            myHeartRates.setAgeYears();
+            myHeartRates.setMaxRate();
+            myHeartRates.setTargetRateMin();
+            myHeartRates.setTargetRateMax();
+
+            System.out.printf("%n~ Your Target Heart Rate Calculator Results ~%n");
+            System.out.printf("%nFirst Name: %s%n", myHeartRates.getFName());
+            System.out.printf("Last Name: %s%n", myHeartRates.getLName());  
+            System.out.printf("Age: %s%n", myHeartRates.getAgeYears());
+            System.out.printf("Maximum Heart Rate: %s%n", myHeartRates.getMaxRate());
+            System.out.printf("Target Heart Rate Range: %n");
+            System.out.printf("\tMinimum: %.0f%n", myHeartRates.getTargetRateMin());
+            System.out.printf("\tMaximum: %.0f%n%n", myHeartRates.getTargetRateMax());
+        input.close();
+    } //end main
+
+} //end HeartRateCalc
+
+
---
+++ Lab_2/HeartRates/src/HeartRates.java
@@ -0,0 +1,112 @@
+/* 
+Student: Clarissa Lacefield
+Class: CS145
+Lab: Lab 2 Heart Rate Calculator
+Extra Credit: set current year automatically 
+*/
+
+public class HeartRates {
+    
+    //class attributes should include fname/lName/yob/current year
+    private String fName;
+    private String lName;
+    private int birthYear;
+    // private int currentYear;
+    private int currentYear = java.time.Year.now().getValue();
+    private int ageYears;
+    private int maxRate;
+    private double targetRateMin;
+    private double targetRateMax;
+
+    //create a constructor the receives data as a parameter
+    //fig 7.5 pg 257, White book & assignment instructions
+    public HeartRates(String fName, String lName, int birthYear, int currentYear) {
+        this.fName = fName;
+        this.lName = lName;
+        this.birthYear = birthYear;
+        this.currentYear = currentYear;
+        getAgeYears();
+        getMaxRate();
+        getTargetRateMin();
+        getTargetRateMax();
+        
+    }
+
+    //use a method to set the first name in the object, account.java examples
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+    //use a method to get the first name in the object
+    public String getFName() {
+        return fName;
+    }
+
+    //use a method to set the last name in the object, account.java examples
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+    //use a method to get the last name in the object
+    public String getLName() {
+        return lName;
+    }
+
+    //use a method to set the birthYear in the object, account.java examples
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+    //use a method to get the birth year in the object
+    public int getBirthYear(){
+        return birthYear;
+    }
+
+    //use a method to set the currentYear in the object, account.java examples
+    public void setCurrentYear(int currentYear) {
+        this.currentYear = currentYear;
+    }
+    //use a method to get the current in the object
+    public int getCurrentYear(){
+        return currentYear;
+    }
+    //use a method to set age
+    public void setAgeYears() {
+        this.ageYears = currentYear - birthYear;
+    }
+
+    public int getAgeYears() {
+        return ageYears;
+    }
+
+    //calculate max heart rate by age, use a method to set it
+    public void setMaxRate() {
+        int age = getAgeYears();
+        this.maxRate = 220 - age;
+    } 
+
+    public int getMaxRate() {
+        return maxRate;
+    }
+    //calculate target heart rate min
+    public void setTargetRateMin() {
+        double targetMin = (double) maxRate;
+        this.targetRateMin = (targetMin * .50); //use double for percentages
+}
+
+    public double getTargetRateMin() {
+        return targetRateMin;
+    }
+
+    //calculate target heart rate max
+    public void setTargetRateMax() {
+        double targetMax = (double) maxRate;
+        this.targetRateMax = (targetMax * .85);
+    }
+
+    public double getTargetRateMax() {
+        return targetRateMax;
+    }
+
+    //empty constructor solves undefined constructor issue per stack overflow
+    public HeartRates() {
+
+    }
+} //end HeartRates
