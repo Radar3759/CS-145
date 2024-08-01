153cd828ef427c4b48708e6f4393903b1834d559
Radar3759 <chainmailgirl@gmail.com>
2024-07-31 17:15:08 -0700

    -fix typo in testclass

--- Lab_5/Phone Book/src/TestClass.java
+++ Lab_5/Phone Book/src/TestClass.java
@@ -9,7 +9,7 @@ public class TestClass {
     System.out.printf("%s %s%n", "Name is:", PhoneBookEntry.getName());
     System.out.printf("%s %s%n", "Address is:", PhoneBookEntry.getAddress());
     System.out.printf("%s %.2f%n", "City is:", PhoneBookEntry.getCity());
-    System.out.printf("%s %.2f%n", "Commission rate is:", PhoneBookEntry.getPhoneNum());
+    System.out.printf("%s %.2f%n", "Phone number is:", PhoneBookEntry.getPhoneNum());
 
 
     System.out.printf("%n%s: %n%n%s%n%n", "Phonebook entry obtained by toString", PhoneBookEntry.toString());
