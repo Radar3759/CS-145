f66d044974fb149322563d6624f2c417e641b4a0
Radar3759 <chainmailgirl@gmail.com>
2024-08-02 16:20:08 -0700

    -add Pascals

--- Lab_6/Recursion/src/Pascals.java
+++ Lab_6/Recursion/src/Pascals.java
@@ -3,6 +3,54 @@ Name: Clarissa Lacefield
 Class: CS145
 Assignment: Lab 6, Pascal's Triangle
 Date: 8/2/2024
+Extra Credit: I made the program check to see if the number entered is between 1-25
+Notes: I used deftstack.com, geeksforgeeks.org, and baeldung.com for pascal formulas in 
+'numY' is a bad var name. I needed it to be not a single char so I could 'see' it
 */
 
 //write a program to print pascal's triangle here
+import java.util.Scanner;
+class Pascals {
+    public static void main(String[] args) {
+        //allow input
+        Scanner input = new Scanner(System.in);
+        boolean win = false; //init while loop to keep asking for the correct number range
+            while (win == false) {
+            //asks for user to input a num between 1-25
+            System.out.printf("%nEnter a number between 1 and 25: ");
+            //assigns the int given by the user to the var 'num'
+            int numY= input.nextInt();
+                if(numY < 1 || numY > 25) {
+                    System.out.printf("%nThe number must be between 1 and 25");
+                } else {
+                    //loop through nums from 1-numY
+                    for (int i = 0; i < numY; i++) {
+                        //prints spaces
+                        for(int k = 1; k < numY - i; k++) {
+                            System.out.print(" ");
+                        }
+                        for (int j = 0; j <= i; j++) {
+                            System.out.print(nCr(i,j)+ " "); //calls the nCr(i,j) method to calc the bionomial coeffiecients and print them
+                        }
+                        System.out.println();//can this just be an n% at the end of 26? Try it
+                        win = true; //close the while loop
+                        input.close(); //closes the Scanner input
+                    }
+                }
+            } //ends while
+    } //ends main
+    //makes the formula for binomial coeffiecients (what pascal's triangle is formed of) work
+    private static int nCr(int n, int r) {
+        int numerator = 1;
+        int denominator = 1;
+        if (n < r || n == 0)
+            return 1;
+
+        for (int s = r; s >= 1; s--) {
+            numerator = numerator * n--;
+            denominator = denominator * s;
+        }
+        return (numerator / denominator);
+    }
+
+}
