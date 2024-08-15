ea67a43991424e9cbf00789ef5e7e8a79d6d23fd
Radar3759 <chainmailgirl@gmail.com>
2024-08-14 17:58:51 -0700

    -update or add TreeTest

--- Lab_7/BinaryPrinting/src/TreeTest.java
+++ Lab_7/BinaryPrinting/src/TreeTest.java
@@ -1,5 +1,33 @@
-public class App {
+import java.security.SecureRandom;
+
+
+//mostly from fig 21.16 in the book
+public class TreeTest {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Tree<Integer> tree = new Tree<Integer>();
+        SecureRandom randomNumber = new SecureRandom();
+
+        System.out.println("A list of ten random numbers: ");
+        for (int i = 1; i<= 10; i++ ){
+            int value = randomNumber.nextInt(100);
+            System.out.printf("%d ", value);
+            tree.insertNode(value);
+        }
+        System.out.printf("%n%nPreorder transversal%n");
+        tree.preorderTraversal();
+
+        System.out.printf("%n%nInorder transversal%n");
+        tree.inorderTraversal();
+
+        System.out.printf("%n%nPostorder traversal%n");
+        tree.postorderTraversal();
+        System.out.println();
+        //not from fig 21.16 
+        tree.insertNode(2);
+        tree.outputTree(0);
+        
+
+    
     }
+
 }
